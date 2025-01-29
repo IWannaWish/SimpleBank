70387bd178b8284ec0a3da4f7fe6f36f5cd3a396
IWannaWish <138825123+IWannaWish@users.noreply.github.com>
2025-01-29 23:57:19 +0300

    add test for transfer and update balance query

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -86,7 +86,21 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		//TODO: updates accounts balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountId,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountId,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
